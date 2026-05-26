c9ee32fe564b3715c86ccb521224866f3bb1d83e
neel-1905 <neelnarwadkar@gmail.com>
2026-05-26 12:25:08 +0530

    routing setup

---
+++ frontend/app/(protected)/chat/[conversationId].tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Conversation() {
+  return (
+    <View>
+      <Text>Conversation</Text>
+    </View>
+  );
+}
